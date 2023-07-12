4d0fc3415fe478872f3f29f4a39f7579d7af62e9
Paul Doazan <paul.doazan@edumedia-sciences.com>
2023-07-12 23:37:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// https://dev.to/emkay860/read-and-rename-all-files-in-a-directory-with-nodejs-2dfo
+
+const fs = require("fs");
+const path = require("path");
+
+const folderPath = "./assets";
+
+
+// read all files in the directory
+let filesArr = fs.readdirSync(folderPath);
+
+
+// Loop through array and rename all files 
+
+filesArr.forEach((file, index) => {
+  let fullPath = path.join(folderPath, file);
+  let fileExtension = path.extname(file);
+  let fileName = path.basename(file, fileExtension);
+
+  let newFileName = "juillet_" + index + "." + fileExtension;
+
+  console.log(fileName)
+try {
+  fs.renameSync(fullPath, path.join(folderPath, newFileName));
+} catch (error) {
+  console.error(error)
+}
+});
